4359256723aa59cb119086ea084e1d14a1091734
kajal-patel97 <57909923+kajal-patel97@users.noreply.github.com>
2019-12-05 19:18:33 +0000

    monster class testing scare_attack passed

--- class_monster_tests.py
+++ class_monster_tests.py
@@ -4,3 +4,9 @@ from class_monster import *
 name ='Paul'
 skills = ['scary', 'hairy', 'loud']
 monster1 = Monster(name, skills)
+
+## Testing scare_attack()
+
+print("checking if monster can scare_attack properly")
+print(monster1.scare_attack() == 'RAAAAHHHHH!!!... RAWWRRRRRR!!!!')
+print("got:", monster1.scare_attack())
